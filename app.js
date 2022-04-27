1aa98808f23a17d9c34e20f3137ec88740bf202a
Mir <65391345+Mir-SA@users.noreply.github.com>
2022-04-27 10:12:18 +0530

    deployment changes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get("/health", (req, res) => res.send("OK!"));
+app.get("/version", (req, res) => res.send("1.1.0"));
+
 app.listen(PORT, () => {
   console.log("server started on port 5000");
 });
